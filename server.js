bef2bdf7ca04db30e99d784051ebccc7863f789c
Nikita Grishin <ng222jk@student.lnu.se>
2023-01-19 15:38:34 +0100

    cors fix

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const cors = require("cors")
 const axios = require("axios")
 
 require("dotenv").config("./.env")
+const CLIENT = "https://lizasil.github.io/Sakamata/"
 const CID = process.env.CHANNEL_ID
 const KEY = process.env.API_KEY
 const PORT = process.env.PORT || 3000
-const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CID}&order=date&key=${KEY}`
+const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CID}&order=date&key=${KEY}&orign=${CLIENT}`
 
 var livestatus
 var updated
 var videoid
 
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    AccessControlAllowOrigin: "*",
+    origin: "*",
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
 
 // Fetch the data with axios
 setInterval(() => {
@@ -31,6 +39,7 @@ app.get("/livestream-status", (req, res) => {
 
 app.listen(PORT, () => {
   getData()
+  console.log(URL)
   console.log(`Server running on port ${PORT}`)
 })
 
